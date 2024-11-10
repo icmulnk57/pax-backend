014f4883b7b5f6609ae64daf60dbc381a37dfbcd
icmulnk57 <mandalamankumar.786@gmail.com>
2024-11-10 14:21:30 +0530

    cors fix

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');  
 const app = express();
 const PORT = 3000;
 
+app.use(cors());  // Enable CORS for all origins
 app.use(express.json());
 
 const cookieParser = require('cookie-parser');
